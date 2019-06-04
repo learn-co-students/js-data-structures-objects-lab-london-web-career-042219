eddd56e90b7e42fe8eeed424c6731b0dc547efce
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-04 20:15:51 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const driver = {}
+
+// The function should not mutate the driver parameter and should return a new driver that has an updated value for the key passed in.
+function updateDriverWithKeyAndValue(object, key, value) {
+  const newObj = { ...object};
+  newObj[key] = value;
+  return newObj;
+}
+
+// it should mutate the driver parameter passed in.
+function destructivelyUpdateDriverWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+// It should delete the key/value pair for the key that was passed in from the driver Object. This should all not actually mutate the driver passed in.
+function deleteFromDriverByKey(object, key) {
+  const newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj
+}
+
+
+// should mutate the driver passed in. Be sure and consider whether dot-notation or bracket-notation might affect your solution.
+function destructivelyDeleteFromDriverByKey(object, key) {
+  delete object[key];
+  return object
+}
